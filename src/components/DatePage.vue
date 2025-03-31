a2ad2ae91f1fd2bb630a96b0217c65aa55e44cb2
gorachu <maksimvrus371@gmail.com>
2025-03-31 18:59:18 +0300

    new hadlers: ChangeWorkoutInfo

--- src/components/DatePage.vue
+++ src/components/DatePage.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="trainings-holder">
     <h2 class="trainings-header">Тренировки за: {{ formattedDate }}</h2>
-      <div class="single-training-holder" v-for="data in jTrainings.trainingsInDay">
+      <div class="single-training-holder" v-for="training in jTrainings.trainingsInDay">
         <div class="header-button-container">  
-          <h4 class="training-time-header">{{ data.timeStart }} - {{ data.timeEnd }}</h4>
-          <button class="edit-button" @click="editClick(data.workoutId)">Редактировать</button>
+          <h4 class="training-time-header">{{ training.timeStart }} - {{ training.timeEnd }}</h4>
+          <button class="edit-button" @click="editClick(training.workoutId)">Редактировать</button>
         </div>
-        <ul class="exercise-holder">
-          <li class="exercsise-info" v-for="training in data.workoutExercises">
-            <span class="custom-marker"></span>{{ training.exerciseName }}
-          </li>
-        </ul>
+        <div class="training-info column">
+          <p class="training-info-row">⏳ Длительность: {{calculateDuration(training.timeStart, training.timeEnd)}}</p>
+          <p class="training-info-row">Количество упражнений: {{training.workoutExercises.length}}</p>
+          <div class="training-info-row column training-text-area">
+            <label>Описание проведенной тренировки:</label>
+            <textarea 
+              v-model="training.notes" 
+              placeholder="Напишите, как прошла тренировка..." 
+              class="training-textarea" 
+              maxlength="800"
+              @change="onInputFinished(training)"></textarea>
+            <p class="char-count" v-if="training.notes?.length">{{ training.notes.length }} / 800</p>
+          </div>
+        </div>
       </div>
   </div>
 </template>
@@ -20,9 +29,11 @@ import { useRouter, useRoute} from 'vue-router'
 import { format } from 'date-fns'
 import { ru } from 'date-fns/locale'
 import { onMounted, ref } from 'vue'
+import '../assets/styles/DatePage.css'
 const jTrainings = ref([]);
 const router = useRouter();
-onMounted(async () => {
+onMounted(loadTrainings)
+async function loadTrainings() {
   try {
     const response = await fetch(`https://localhost:8443/calendar/${route.params.date}`,  {
       method: 'GET',
@@ -39,7 +50,7 @@ onMounted(async () => {
   } catch (error){
     console.error('Ошибка:', error)
   }
-})
+}
 const route = useRoute()
 const currentYear = new Date().getFullYear()
 const date = new Date(route.params.date)
@@ -52,4 +63,42 @@ if (date.getFullYear() === currentYear) {
 function editClick(workoutId) {
   router.push(`${route.fullPath}/${workoutId}`);
 }
+function calculateDuration(start, end) {
+  const startTime = new Date(`1970-01-01T${start}Z`);
+  let endTime = new Date(`1970-01-01T${end}Z`);
+  if (startTime > endTime) {
+    endTime = new Date(`1970-01-02T${end}Z`);
+  }
+  const durationInMs = endTime - startTime;
+  const hours = Math.floor(durationInMs / (1000 * 60 * 60));
+  const minutes = Math.floor((durationInMs % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((durationInMs % (1000 * 60)) / 1000);
+
+  return `${hours} ч ${minutes} мин ${seconds} сек`;
+}
+async function onInputFinished(training){
+  console.log("Input finished:", JSON.stringify({
+          notes: training.notes,
+      }),);
+  try {
+    const response = await fetch(`https://localhost:8443/calendar/workouts/${training.workoutId}`,  {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+          notes: training.notes,
+      }),
+      credentials: 'include',
+    })
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки тренировок')
+    } else {
+      loadTrainings();
+    }
+  } catch (error){
+    console.error('Ошибка:', error)
+  }
+}
+
 </script>
